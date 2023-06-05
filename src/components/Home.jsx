5df9f54ac5852a0b728beb5e90d75191a6398bf3
unknown <ebussman@gmail.com>
2023-06-05 16:36:50 -0700

    added some styling for mobile, currently made the bg image just hidden

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col bg-gray-100">
-        <div className="flex flex-col text-white  recital-bg">
+        <div className="mt-8 md:p-16 flex flex-col justify-center md:justify-start text-white recital-bg">
           <h1 className="flex justify-center">Cecilia Poellein Buss</h1>
-          <h1 className="flex justify-center">
-            Violinist/ Violin and Viola Instructor
+          <h1 className="flex text-center justify-center">
+            Violinist/Violin and Viola Instructor
           </h1>
         </div>
         <p>
@@ -32,19 +32,19 @@ export default function Home() {
           State Orchestras and Washington Solo Ensemble In addition to private
           teaching Cecilia has coached high school and middle school orchestras
           in Seattle and on the Eastside. When she is not teaching, practicing
-          or playing Cecilia enjoys bike riding, walking, Pilates Classes and
+          or playing violin, Cecilia enjoys bike riding, walking, Pilates Classes and
           travelling with her husband, Gerry.
         </p>
-        <div className="flex flex-col items-center justify-center" id="contact">
-          <h2>
+        <div className="mx-4 flex flex-col items-center justify-center" id="contact">
+          <h2 className="mb-5">
             Interested in taking you or your child's violin skills to the next
             level?
           </h2>
-          <h2>
+          <h2 className="mb-5">
             Contact me for a Consultation! Please send a 2 minute audio
             recording of a piece, and include one scale.
           </h2>
-          <h2>Email: Cmpbuss@comcast.net</h2>
+          <h2 className="mb-5">Email: Cmpbuss@comcast.net</h2>
         </div>
       </div>
     </>
